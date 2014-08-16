f6df89a186c8752b00177fa9ce057c948f53b75d
sauravtom <stomatrix@gmail.com>
2014-08-16 10:41:16 +0530

    Now sending links as well with YO notification.

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ class AlreadyYoyed(db.Expando):
     url = db.StringProperty()
 
 class Cron(webapp2.RequestHandler):
-    def send_yo(self):
-        data = {'api_token':api_token}
+    def send_yo(self,link):
+        data = {'api_token':api_token,'link':link}
         data = urllib.urlencode(data)
         request_object = urllib2.Request('http://api.justyo.co/yoall/', data)
         response = urllib2.urlopen(request_object)
@@ -24,14 +24,17 @@ class Cron(webapp2.RequestHandler):
     def get(self):
         hn_url = 'http://hnify.herokuapp.com/get/top'
         response = json.loads(urllib2.urlopen(hn_url).read())
-        top = [i for i in response['stories'] if i['points'] > 500][0]
+        try:
+            top = [i for i in response['stories'] if i['points'] > 500][0]
+        except IndexError:
+            return
         if top:
             print top['link']
             already_yoyed = [instance.url for instance in AlreadyYoyed.all().fetch(1000000)]
             print already_yoyed
             if top['link'] not in already_yoyed:
                 self.add_to_db(top['link'])
-                self.send_yo()
+                self.send_yo(top['link'])
             
 application = webapp2.WSGIApplication([
     ('/cron', Cron)
